667b6c41bbd3e7e16b3c8727c4a2007c518a5b35
eva <hanzlikova.eva@seznam.cz>
2022-11-01 18:59:09 +0100

    Data input chosing
    možnost vybrat vstupu dat od user, ze složky nebo od jinud

--- label_maker.py
+++ label_maker.py
@@ -4,7 +4,7 @@ setup_logging()
 import logging
 
 from calc import calculate_unit_price
-from inputs import csv_input
+from inputs import csv_input, user_input
 from outputs import to_word
 
 log = logging.getLogger(__name__)
@@ -13,10 +13,21 @@ log = logging.getLogger(__name__)
 def main():
     log.info(' program start '.center(80, '-'))
 
-    # TODO: vyměnit za argument od argparse
-    file_path = "input/sample_data.csv"
+    data_input = "user"
+    data = []
 
-    data = csv_input()
+    if data_input == "user":
+        data = user_input()
+    elif data_input == "csv":
+        # TODO: vyměnit za argument od argparse
+        file_path = "input/sample_data.csv"
+        data = csv_input(file_path)
+    else:
+        log.warning(f"neznám možnost vstupu: {data_input}")
+        exit()
+        # print("neznám")
+
+
     calculated_data = calculate_unit_price(data)
     to_word(calculated_data, 'templates/labels_template.docx')
     log.info(' program end '.center(80, '-'))
